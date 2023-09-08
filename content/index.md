335ad01502a683067e1d9a4d99abaac7972bce62
jaywang <jaywang@leadinfo.com.tw>
2023-09-08 17:02:03 +0800

    Quartz sync: Sep 8, 2023, 5:02 PM

--- content/index.md
+++ content/index.md
@@ -1,6 +1,5 @@
 ---
-title: Welcome to Quartz
+title: Root
 ---
 
-This is a blank Quartz installation.
-See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+Hello world!
--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -11,9 +11,7 @@ export default (() => {
     const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
-
-    const iconPath = joinSegments(baseDir, "static/icon.png")
-    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
+    const ogImagePath = joinSegments(baseDir, "static/og-image.png")
 
     return (
       <head>
@@ -25,7 +23,10 @@ export default (() => {
         {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
         <meta property="og:width" content="1200" />
         <meta property="og:height" content="675" />
-        <link rel="icon" href={iconPath} />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎮</text></svg>"
+        />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
